6db5dbd244f4d28aa1a691a59a5a7b1eea9262b2
Ben Wattelman <benwattelman6@gmail.com>
2021-12-03 15:48:11 +0200

    added connect function

--- get_data.py
+++ get_data.py
@@ -1,4 +1,8 @@
+import MySQLdb as mdb
+
+db_code = "DbMysql26"
 tmdb_api_key = "c722ee1702b642f1eb03bbbb186ea45b"
 example_tmdb_api_request = "https://api.themoviedb.org/3/movie/550?api_key=c722ee1702b642f1eb03bbbb186ea45b"
 
+con = mdb.connect('localhost', db_code, db_code, db_code)
 
