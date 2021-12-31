ed15f70e8de7fa3c661331595188964781f32bdd
Ben Wattelman <benwattelman6@gmail.com>
2021-12-31 12:21:29 +0200

    query 1 done

--- SRC/QUERIES/queries.sql
+++ SRC/QUERIES/queries.sql
@@ -1,11 +1,19 @@
--- query 1 - competition across genres !! NOT_DONE_YET !!
-SELECT pc.id as ID, pc.name as PRODUCTION_COMPANY_NAME, SUM(m.popularity) /*did this by popularity, there are other options we should consider*/
-FROM movies as m, production_companies as pc, movies_production_companies as mpc, genres as g, movies_genres as mg
-WHERE m.id = mpc.movie_id
-and pc.id = mpc.production_company_id
-and m.id = mg.movie_id
-and g.id = mg.genre_id
-GROUP BY g.id, pc.id as production_companies_popularity --todo: validate this
+-- query 1 - competition across genres
+SELECT genre, production_company, popularity
+FROM (
+	SELECT production_companies_genres_popularity.GENRE as genre, production_companies_genres_popularity.PRODUCTION_COMPANY_NAME as production_company, production_companies_genres_popularity.production_companies_average_popularity_in_genre as popularity,
+	row_number() over (partition by GENRE order by production_companies_average_popularity_in_genre desc) AS ranking
+	FROM (
+		SELECT g.name as GENRE, pc.name as PRODUCTION_COMPANY_NAME, AVG(m.popularity) as production_companies_average_popularity_in_genre
+		FROM movies as m, production_companies as pc, movies_production_companies as mpc, genres as g, movie_genres as mg
+		WHERE m.id = mpc.movie_id
+		and pc.id = mpc.production_company_id
+		and m.id = mg.movie_id
+		and g.id = mg.genre_id
+		GROUP BY g.id, pc.id) AS production_companies_genres_popularity
+	) AS ranking_production_companies_genres
+WHERE ranking <= 3
+ORDER BY genre, production_company
 
 -- query 2 - currently planned film across genres
 SELECT g.name as GENRE, count(*) as NUMBER_OF_PLANNED_MOVIES
